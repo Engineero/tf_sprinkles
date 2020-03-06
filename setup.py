d3783ed792a40d86495ef8de11ea299ca33aba2a
Engineero <engineerolabs@gmail.com>
2020-03-06 16:26:31 -0700

    Version 1.0 ready to go

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from distutils.util import convert_path
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+version_dict = {}
+version_path = convert_path('sprinkles/_version.py')
+
+# Get the version number from the version path.
+with open(version_path, 'r') as ver_file:
+    exec(ver_file.read(), version_dict)
+
+# Get the long description from the README file.
+with open(path.join(here, 'README.md')) as a_file:
+    long_description = a_file.read()
+
+CLASSIFIERS = ['Development Status :: 5 - Production/Stable',
+               'Environment :: Console',
+               'Intended Audience :: Developers',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: MIT License',
+               'Natural Language :: English',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 3.7',
+               'Topic :: Scientific/Engineering',
+               'Topic :: Scientific/Engineering :: Artificial Intelligence',
+               'Topic :: Utilities']
+PROJECT_URLS = {'Documentation': 'https://engineero.github.io/sprinkles',
+                'Source': 'https://github.com/Engineero/sprinkles',
+                'Tracker': 'https://github.com/Engineero/sprinkles/issues'}
+INSTALL_REQUIRES = ['tensorflow>=2.0']
+
+setup(name='sprinkles',
+      packages=['sprinkles'],
+      version=version_dict['__version__'],
+      description='A fast and efficient implimentation of progressive sprinkles augmentation.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Engineero',
+      author_email='engineerolabs@gmail.com',
+      url='https://github.com/Engineero/sprinkles',
+      project_urls=PROJECT_URLS,
+      keywords=['augmentation sprinkles tensorflow'],
+      classifiers=CLASSIFIERS,
+      install_requires=INSTALL_REQUIRES)
