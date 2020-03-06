a988dcc48e13ab28906eefaf5d385c2032feb834
Engineero <engineerolabs@gmail.com>
2020-03-06 16:36:08 -0700

    Updated name to tf_sprinkles, version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 version_dict = {}
-version_path = convert_path('sprinkles/_version.py')
+version_path = convert_path('tf_sprinkles/_version.py')
 
 # Get the version number from the version path.
 with open(version_path, 'r') as ver_file:
@@ -27,20 +27,20 @@ CLASSIFIERS = ['Development Status :: 5 - Production/Stable',
                'Topic :: Scientific/Engineering',
                'Topic :: Scientific/Engineering :: Artificial Intelligence',
                'Topic :: Utilities']
-PROJECT_URLS = {'Documentation': 'https://engineero.github.io/sprinkles',
-                'Source': 'https://github.com/Engineero/sprinkles',
-                'Tracker': 'https://github.com/Engineero/sprinkles/issues'}
+PROJECT_URLS = {'Documentation': 'https://engineero.github.io/tf_sprinkles',
+                'Source': 'https://github.com/Engineero/tf_sprinkles',
+                'Tracker': 'https://github.com/Engineero/tf_sprinkles/issues'}
 INSTALL_REQUIRES = ['tensorflow>=2.0']
 
-setup(name='sprinkles',
-      packages=['sprinkles'],
+setup(name='tf_sprinkles',
+      packages=['tf_sprinkles'],
       version=version_dict['__version__'],
       description='A fast and efficient implimentation of progressive sprinkles augmentation.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Engineero',
       author_email='engineerolabs@gmail.com',
-      url='https://github.com/Engineero/sprinkles',
+      url='https://github.com/Engineero/tf_sprinkles',
       project_urls=PROJECT_URLS,
       keywords=['augmentation sprinkles tensorflow'],
       classifiers=CLASSIFIERS,
