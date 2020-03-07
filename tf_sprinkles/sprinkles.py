e8178f193a74ef75a6c991a33bc425a06ee9e971
Engineero <engineerolabs@gmail.com>
2020-03-06 21:26:22 -0700

    Implemented salt and pepper noise

--- tf_sprinkles/sprinkles.py
+++ tf_sprinkles/sprinkles.py
@@ -17,28 +17,49 @@ class Sprinkles:
         side_length: lenght of sides each hole will have.
 
     Keyword Args:
-        gaussian: if True, fills sprinkles with Gaussian noise. Default is
-            False.
+        mode: one of [None, 'gaussian', 'salt_pepper']. If None, all sprinkles
+            will be black. If 'gaussian', sprinkles will be filled with
+            Gaussian noise. If 'salt_pepper', sprinkles will be randomly
+            black or white. Default is None.
 
     Returns:
         Image with number of holes of specified size cut out.
     """
 
-    def __init__(self, num_holes, side_length, gaussian=False):
-        self.n = num_holes
+    def __init__(self, num_holes, side_length, mode=None):
+        if mode is 'salt_pepper':
+            self.n = num_holes // 2
+        else:
+            self.n = num_holes
         self.length = side_length
-        self.gaussian = gaussian
+        self.mode = mode
 
     def __call__(self, image):
         tf.cast(image, tf.float32)
         img_shape = tf.shape(image)
-        if self.gaussian:
-            rejected = tf.random.normal(img_shape, dtype=tf.float32)
-        else:
+        if self.mode is None:
             rejected = tf.zeros_like(image)
+        elif self.mode is 'gaussian':
+            rejected = tf.random.normal(img_shape, dtype=tf.float32)
+        elif self.mode is 'salt_pepper':
+            rejected_high = tf.ones_like(image)
+            rejected_low = tf.zeros_like(image)
+        else:
+            raise ValueError(f'Unknown mode "{self.mode}" given.')
         rows = img_shape[0]
         cols = img_shape[1]
         num_channels = img_shape[-1]
+        if self.mode is 'salt_pepper':
+            mask1 = self._make_mask(rows, cols, num_channels)
+            mask2 = self._make_mask(rows, cols, num_channels)
+            filtered_image = tf.where(mask1, rejected_high, image)
+            filtered_image = tf.where(mask2, rejected_low, filtered_image)
+        else:
+            mask = self._make_mask(rows, cols, num_channels)
+            filtered_image = tf.where(mask, rejected, image)
+        return filtered_image
+
+    def _make_mask(self, rows, cols, num_channels):
         row_range = tf.tile(tf.range(rows)[..., tf.newaxis], [1, self.n])
         col_range = tf.tile(tf.range(cols)[..., tf.newaxis], [1, self.n])
         r_idx = tf.random.uniform([self.n], minval=0, maxval=rows-1,
@@ -57,6 +78,4 @@ class Sprinkles:
         mask = tf.reduce_any(mask, axis=-1)
         mask = mask[..., tf.newaxis]
         mask = tf.tile(mask, [1, 1, num_channels])
-
-        filtered_image = tf.where(mask, rejected, image)
-        return filtered_image
+        return mask
